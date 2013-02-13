783888f91aea1b457da8d347d0e74844497cd741
dandrews <daniel.raymond.andrews@gmail.com>
2013-02-12 21:21:04 -0700

    limit max, min, and mean to data of the last 30 years

--- guesses.py
+++ guesses.py
@@ -4,9 +4,11 @@
 import random
 import datetime
 
+FMT = '%m/%d/%Y %I:%M %p'
+
 def get_breakups():
     f = open('nenana_converted.txt')
-    breakups = {}
+    breakups = []
     for line in iter(f):
         line_array = line.split(',')
         date = line_array[0]
@@ -14,69 +16,62 @@ def get_breakups():
         day = date.split()[1]
         clock_time = date[(len(day) + 1 + len(month) + 1):]
         year = line_array[1].strip("\n")
-        breakups[year] = "%s/%s/%s %s" % (month, day, year, clock_time)
+        breakup_date = "%s/%s/%s %s" % (month, day, year, clock_time)
+        breakups.append( datetime.datetime.strptime(breakup_date, FMT) )
     f.close()
     return breakups
 
 breakups = get_breakups()
 
-FMT = '%m/%d/%Y %I:%M %p'
+YEAR = 2011
 
-YEAR = '2009'
-ACTUAL = breakups[YEAR]
-ACTUAL = datetime.datetime.strptime(ACTUAL, FMT)
+index = map(lambda d: d.year, breakups).index(YEAR)
+ACTUAL = breakups[index]
 
-MIN = "4/20/%s 4:54 PM" % YEAR
-MAX = "5/20/%s 11:41 AM" % YEAR
+breakups = breakups[-30:]
 
-MAX = datetime.datetime.strptime(MAX, FMT)
-MIN = datetime.datetime.strptime(MIN, FMT)
+breakups.sort(key = lambda d: (d.month, d.day))
+
+MIN = breakups[0].replace( year=YEAR)
+MAX = breakups[-1].replace( year=YEAR)
+
 DIFF = MAX - MIN
 MINUTES = divmod(DIFF.total_seconds(), 60)[0]
 MEAN = MIN + datetime.timedelta(minutes=MINUTES/2)
 
-# print MEAN.strftime(FMT)
+print
+print "MIN: %s" % MIN.strftime(FMT)
+print "MEAN: %s" % MEAN.strftime(FMT)
+print "MAX: %s" % MAX.strftime(FMT)
+print
 
 def make_guess():
     guess_delta = (random.gauss(0,.5) * MINUTES/4)
     guess = MEAN + datetime.timedelta(minutes=guess_delta)
     return guess.replace( second=0, microsecond=0)
 
+MAX_GUESSES = 1000
 random_datetimes = []
-guesses = 0
-while guesses < 1000:
+while len(random_datetimes) < MAX_GUESSES:
     guess = make_guess()
     # want 1000 unique guesses
     if guess not in random_datetimes:
         random_datetimes.append( guess )
-        guesses++
 
 guess_diffs = []    
 for guess in random_datetimes:
-    # print guess.strftime(FMT) 
     guess_diff = guess - ACTUAL
     guess_minutes = divmod(guess_diff.total_seconds(), 60)[0]
-    # print guess_minutes
     guess_diffs.append( abs(guess_minutes) )
 
 average = sum( guess_diffs ) / len(guess_diffs)
 
-# print    
-# print "off by"
-# print average
-# print "minutes"
-# print
-# print "off by"
-# print average / 60 
-# print "hours"    
-# print
-# print "worst guess:"
-# print max(guess_diffs)
 print
-print "closest guess:"
+print "CLOSEST GUESS:"
 print min(guess_diffs)
 print ACTUAL.strftime(FMT) 
 print ACTUAL in random_datetimes
+print
 
 if ACTUAL in random_datetimes:
     guesses_file = open( 'guesses.txt', 'w' )
